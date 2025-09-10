0c216cafca70741ccafe6734dc8684c29c38a933
gwongilnam <nimpa3201@naver.com>
2025-09-10 14:27:56 +0900

    feat: volatile,메모리 가시성2

--- src/thread/volatile1/VolatileFlagMain.java
+++ src/thread/volatile1/VolatileFlagMain.java
@@ -18,10 +18,9 @@ public class VolatileFlagMain {
         log("main 종료");
 
 
-
     }
 
-    static class MyTask implements Runnable{
+    static class MyTask implements Runnable {
 
         boolean runFlag = true;
         //volatile boolean runFlag = true;
@@ -29,11 +28,22 @@ public class VolatileFlagMain {
         @Override
         public void run() {
             log("task 시작");
-            while(runFlag){
+            while (runFlag) {
                 // runFlag가 false로 변하면 탈출
             }
             log("task 종료");
 
         }
+
+        /**
+         *  13:50:45:024 [     main] runFlag = true
+         *  13:50:45:043 [     work] task 시작
+         *  13:50:46:047 [     main] runFlag를 false로 변경 시도
+         *  13:50:46:048 [     main] runFlag = false
+         */
+        /* work 스레드 종료 안됨 */
     }
 }
+
+    //캐시 메모리의 runFlag 값만 변한다는 것이다! 메인 메모리에 이 값이 즉시 반영되지 않는다.
+
