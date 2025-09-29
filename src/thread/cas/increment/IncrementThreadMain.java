97ed1cdb834ac726d74e85a7f190c6943de96c50
gwongilnam <nimpa3201@naver.com>
2025-09-29 17:53:04 +0900

    feat: 원자적 연산 - volatile,synchronized

--- src/thread/cas/increment/IncrementThreadMain.java
+++ src/thread/cas/increment/IncrementThreadMain.java
@@ -9,6 +9,8 @@ public class IncrementThreadMain {
 
     public static void main(String[] args) throws InterruptedException {
         test(new BasicInteger());
+        test(new VolatileInteger());
+        test(new SyncInteger());
 
     }
 
@@ -38,6 +40,11 @@ public class IncrementThreadMain {
         int result = incrementInteger.get();
         System.out.println(incrementInteger.getClass().getSimpleName() + ": " + result);
         //BasicInteger: 991 1000이 아니라 다른 숫자가 보인다. 스레드가 동시에 원자적이지 않은 value++를 호출했기 때문
+        /**
+         * BasicInteger: 993
+         * VolatileInteger: 996
+         * SyncInteger: 1000
+         */
     }
 
 }
