2c8ca01697ba8676601b55342863931eea8757e2
gwongilnam <nimpa3201@naver.com>
2025-10-02 18:56:35 +0900

    feat: CAS 연산1

--- src/thread/cas/increment/IncrementPerformanceMain.java
+++ src/thread/cas/increment/IncrementPerformanceMain.java
@@ -30,6 +30,6 @@ public class IncrementPerformanceMain {
      * BasicInteger: ms=343  CPU 캐시를 적극 활용하고 CPU 캐시의 위력을 알 수 있다 / 단일 스레드가 사용하는 경우 효율적
      * VolatileInteger: ms=889 원자적 연산일때는 유용 / 하지만 value++ 소용없음
      * SyncInteger: ms=2434   안전하지만 MyAtomicInteger 보다 느림
-     * MyAtomicInteger: ms=903 synchronized , ReentrantLock 을 사용하는 경우보다 1,5 ~ 2배 정도 빠르다 -> 락을 사용하지 않고, 원잦거 연산을 만들어냄
+     * MyAtomicInteger: ms=903 synchronized , ReentrantLock 을 사용하는 경우보다 1,5 ~ 2배 정도 빠르다 -> 락을 사용하지 않고, 원자적 연산을 만들어냄
      */
 }
