7650bb8ab462712e2201e4ec047081dab065ea7a
gwongilnam <nimpa3201@naver.com>
2025-10-06 07:27:37 +0900

    feat: 동시성 컬렉션이 필요한 이유1 - 시작

---
+++ src/thread/collection/simple/SimpleListMainV0.java
@@ -0,0 +1,14 @@
+package thread.collection.simple;
+
+import java.util.ArrayList;
+
+public class SimpleListMainV0 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+        // 스레드1, 스레드2가 동시에 실행 가정
+        list.add("A"); // 스레드 1 실행 가정
+        list.add("B"); // 스레드 2 실행 가정
+        System.out.println("list = " + list);
+    }
+}
