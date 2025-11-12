ceaa5a2e4999663c6dabc99b5a32611be6ddde3e
gwongilnam <nimpa3201@naver.com>
2025-11-12 12:31:26 +0900

    feat: Executor 전략 - 사용자 정의 풀 전략

---
+++ src/thread/excutor/poolsize/PoolSizeMainV4.java
@@ -0,0 +1,41 @@
+package thread.excutor.poolsize;
+
+import thread.excutor.RunnableTasks;
+
+import java.util.concurrent.*;
+
+import static thread.excutor.ExecutorUtils.printState;
+import static util.MyLogger.log;
+
+public class PoolSizeMainV4 {
+
+    //static final int TASK_SIZE = 1100; // 1. 일반
+
+    //static final int TASK_SIZE = 1200; // 2. 긴급
+    static final int TASK_SIZE = 1201; // 3. 거절
+    public static void main(String[] args) {
+
+        ExecutorService es = new ThreadPoolExecutor(100, 200, 60, TimeUnit.SECONDS, new ArrayBlockingQueue<>(1000));
+        printState(es);
+
+        long startMs = System.currentTimeMillis();
+
+        for (int i = 1; i <= TASK_SIZE; i++) {
+            String taskName = "task" + i;
+            try{
+                es.execute(new RunnableTasks(taskName));
+                printState(es, taskName);
+            }catch (RejectedExecutionException e){
+                log(taskName+ " -> " + e);
+            }
+
+        }
+
+
+        es.close();
+        long endMs = System.currentTimeMillis();
+        log("time: " + (endMs - startMs));
+
+
+    }
+}
